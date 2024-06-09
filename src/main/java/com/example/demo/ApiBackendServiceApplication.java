35946b93d76680a05b6559f820241eb0c221f056
HariKrishnaGade <krish.harry2024@gmail.com>
2024-06-08 22:15:24 -0700

    Added controller

--- src/main/java/com/example/demo/ApiBackendServiceApplication.java
+++ src/main/java/com/example/demo/ApiBackendServiceApplication.java
@@ -2,8 +2,12 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import com.example.controller.CustomerController;
 
 @SpringBootApplication
+@ComponentScan(basePackageClasses = CustomerController.class)
 public class ApiBackendServiceApplication {
 
 	public static void main(String[] args) {
